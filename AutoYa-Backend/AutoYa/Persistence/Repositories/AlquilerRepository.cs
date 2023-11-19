a294ecb4c35b8e4e9ebcc5895791ef6d1a9eba34
Johan Moreno Vergara <U20201C105@UPC.EDU.PE>
2023-11-19 18:48:17 -0500

    XML Documentation - ArrendatarioRepository

--- AutoYa-Backend/AutoYa/Persistence/Repositories/AlquilerRepository.cs
+++ AutoYa-Backend/AutoYa/Persistence/Repositories/AlquilerRepository.cs
@@ -4,49 +4,106 @@ using AutoYa_Backend.Shared.Persistence.Contexts;
 using AutoYa_Backend.Shared.Persistence.Repositories;
 using Microsoft.EntityFrameworkCore;
 
+/// <summary>
+/// Espacio de nombres para las implementaciones de repositorios de AutoYa_Backend.
+/// </summary>
+
 namespace AutoYa_Backend.AutoYa.Persistence.Repositories;
+
+/// <summary>
+/// Repositorio para manejar las operaciones de la entidad Alquiler.
+/// </summary>
 
 public class AlquilerRepository : BaseRepository, IAlquilerRepository
 {
+    /// <summary>
+    /// Inicializa una nueva instancia de la clase AlquilerRepository con el contexto de base de datos especificado.
+    /// </summary>
+    /// <param name="context">El contexto de base de datos a utilizar.</param>
+    
     public AlquilerRepository(AppDbContext context) : base(context)
     {
     }
+
+    /// <summary>
+    /// Lista asincrónicamente todos los alquileres.
+    /// </summary>
+    /// <returns>Una colección de alquileres.</returns>
 
     public async Task<IEnumerable<Alquiler>> ListAsync()
     {
         return await _context.Alquileres.ToListAsync();
     }
 
+    /// <summary>
+    /// Agrega asincrónicamente un nuevo alquiler al contexto de la base de datos.
+    /// </summary>
+    /// <param name="alquiler">El alquiler a agregar.</param>
+    
     public async Task AddAsync(Alquiler alquiler)
     {
         await _context.Alquileres.AddAsync(alquiler);
     }
+
+     /// <summary>
+    /// Encuentra asincrónicamente un alquiler por su identificador.
+    /// </summary>
+    /// <param name="alquilerId">El identificador del alquiler.</param>
+    /// <returns>El alquiler encontrado o null si no se encuentra.</returns>
 
     public async Task<Alquiler> FindByIdAsync(int? alquilerId)
     {
         return await _context.Alquileres.FindAsync(alquilerId);
     }
 
+    /// <summary>
+    /// Actualiza un alquiler existente en el contexto de la base de datos.
+    /// </summary>
+    /// <param name="alquiler">El alquiler a actualizar.</param>
+    
     public void Update(Alquiler alquiler)
     {
         _context.Alquileres.Update(alquiler);
     }
 
+    /// <summary>
+    /// Elimina un alquiler del contexto de la base de datos.
+    /// </summary>
+    /// <param name="alquiler">El alquiler a eliminar.</param>
+    
     public void Remove(Alquiler alquiler)
     {
         _context.Alquileres.Remove(alquiler);
     }
 
+    /// <summary>
+    /// Encuentra asincrónicamente todos los alquileres relacionados con un vehículo específico por su identificador.
+    /// </summary>
+    /// <param name="vehiculoId">El identificador del vehículo asociado a los alquileres.</param>
+    /// <returns>Una colección de alquileres.</returns>
+    
     public async Task<IEnumerable<Alquiler>> FindByVehiculoIdAsync(int vehiculoId)
     {
         return await _context.Alquileres.Where(a => a.VehiculoId == vehiculoId).ToListAsync();
     }
 
+    /// <summary>
+    /// Encuentra asincrónicamente todos los alquileres relacionados con un propietario específico por su identificador.
+    /// </summary>
+    /// <param name="propietarioId">El identificador del propietario asociado a los alquileres.</param>
+    /// <returns>Una colección de alquileres.</returns>
+    
     public async Task<IEnumerable<Alquiler>> FindByPropietarioIdAsync(int propietarioId)
     {
         return await _context.Alquileres.Where(a => a.PropietarioId == propietarioId).ToListAsync();
     }
 
+    /// <summary>
+    /// Encuentra asincrónicamente todos los alquileres relacionados con un arrendatario específico por su identificador.
+    /// </summary>
+    /// <param name="arrendatarioId">El identificador del arrendatario asociado a los alquileres.</param>
+    /// <returns>Una colección de alquileres.</returns>
+    
     public async Task<IEnumerable<Alquiler>> FindByArrendatarioIdAsync(int arrendatarioId)
     {
         return await _context.Alquileres.Where(a => a.ArrendatarioId == arrendatarioId).ToListAsync();
--- AutoYa-Backend/AutoYa/Persistence/Repositories/ArrendatarioRepository.cs
+++ AutoYa-Backend/AutoYa/Persistence/Repositories/ArrendatarioRepository.cs
@@ -6,37 +6,79 @@ using Microsoft.EntityFrameworkCore;
 
 namespace AutoYa_Backend.AutoYa.Persistence.Repositories;
 
+/// <summary>
+/// Repositorio para manejar las operaciones CRUD de los arrendatarios.
+/// </summary>
+
 public class ArrendatarioRepository : BaseRepository, IArrendatarioRepository
 {
+
+    /// <summary>
+    /// Inicializa una nueva instancia del repositorio ArrendatarioRepository utilizando el contexto de la aplicación proporcionado.
+    /// </summary>
+    /// <param name="context">Contexto de la base de datos utilizado para las operaciones de repositorio.</param>
+    
     public ArrendatarioRepository(AppDbContext context) : base(context)
     {
     }
 
+    /// <summary>
+    /// Obtiene una lista asincrónica de todos los arrendatarios.
+    /// </summary>
+    /// <returns>Una tarea que representa la operación asincrónica, que contiene la lista de arrendatarios.</returns>
+    
     public async Task<IEnumerable<Arrendatario>> ListAsync()
     {
         return await _context.Arrendatarios.ToListAsync();
     }
 
+    /// <summary>
+    /// Agrega un nuevo arrendatario de manera asincrónica al contexto de la base de datos.
+    /// </summary>
+    /// <param name="arrendatario">El arrendatario a agregar.</param>
+    
     public async Task AddAsync(Arrendatario arrendatario)
     {
         await _context.Arrendatarios.AddAsync(arrendatario);
     }
 
+    /// <summary>
+    /// Busca un arrendatario por su identificador de manera asincrónica.
+    /// </summary>
+    /// <param name="arrendatarioId">El identificador del arrendatario a buscar.</param>
+    /// <returns>Una tarea que representa la operación asincrónica, que contiene el arrendatario encontrado o null.</returns>
+    
     public async Task<Arrendatario> FindByIdAsync(int? arrendatarioId)
     {
         return await _context.Arrendatarios.FindAsync(arrendatarioId);
     }
 
+    /// <summary>
+    /// Busca un arrendatario por su correo electrónico de manera asincrónica.
+    /// </summary>
+    /// <param name="email">El correo electrónico del arrendatario a buscar.</param>
+    /// <returns>Una tarea que representa la operación asincrónica, que contiene el arrendatario encontrado o null.</returns>
+    
     public async Task<Arrendatario> FindByEmailAsync(string email)
     {
         return await _context.Arrendatarios.FirstOrDefaultAsync(a => a.Correo == email);
     }
 
+    /// <summary>
+    /// Actualiza los datos de un arrendatario existente en el contexto de la base de datos.
+    /// </summary>
+    /// <param name="arrendatario">El arrendatario a actualizar.</param>
+    
     public void Update(Arrendatario arrendatario)
     {
         _context.Arrendatarios.Update(arrendatario);
     }
 
+    /// <summary>
+    /// Elimina un arrendatario del contexto de la base de datos.
+    /// </summary>
+    /// <param name="arrendatario">El arrendatario a eliminar.</param>
+    
     public void Remove(Arrendatario arrendatario)
     {
         _context.Arrendatarios.Remove(arrendatario);
