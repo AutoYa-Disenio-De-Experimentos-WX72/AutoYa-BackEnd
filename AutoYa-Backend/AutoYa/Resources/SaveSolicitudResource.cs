86d8a2eca87109bb14f4f4a598fee5f51d58a8a2
Johan Moreno Vergara <U20201C105@UPC.EDU.PE>
2023-11-19 20:38:52 -0500

    XML Documentation - SaveSolicitudResource

--- AutoYa-Backend/AutoYa/Resources/SaveSolicitudResource.cs
+++ AutoYa-Backend/AutoYa/Resources/SaveSolicitudResource.cs
@@ -1,7 +1,18 @@
 ﻿namespace AutoYa_Backend.AutoYa.Resources;
 
+/// <summary>
+/// Clase de recurso para guardar información sobre una solicitud.
+/// Utilizada para capturar datos que serán almacenados en la base de datos.
+/// </summary>
 public class SaveSolicitudResource
 {
+    /// <summary>
+    /// Contenido o cuerpo de la solicitud.
+    /// </summary>
     public string Body { get; set; }
+
+    /// <summary>
+    /// Identificador del alquiler asociado con la solicitud.
+    /// </summary>
     public int AlquilerId { get; set; }
 }
