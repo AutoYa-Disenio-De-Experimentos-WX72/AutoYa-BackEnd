842bd43fb67ca2b73ac95f45c54b0291298d905e
Johan Moreno Vergara <U20201C105@UPC.EDU.PE>
2023-11-19 21:47:05 -0500

    XML Documentation - MantenimientoService

--- AutoYa-Backend/AutoYa/Services/MantenimientoService.cs
+++ AutoYa-Backend/AutoYa/Services/MantenimientoService.cs
@@ -6,32 +6,62 @@ using AutoYa_Backend.Shared.Persistence.Repositories;
 
 namespace AutoYa_Backend.AutoYa.Services;
 
+/// <summary>
+/// Servicio que ofrece métodos para gestionar las operaciones CRUD de mantenimientos.
+/// </summary>
 public class MantenimientoService : IMantenimientoService
 {
     private readonly IMantenimientoRepository _mantenimientoRepository;
         private readonly IUnitOfWork _unitOfWork;
 
+
+        // Declaraciones de los repositorios y unidad de trabajo (UnitOfWork)
+        
+        /// <summary>
+        /// Inicializa una nueva instancia del servicio de mantenimientos, inyectando las dependencias necesarias.
+        /// </summary>
+        /// <param name="mantenimientoRepository">Repositorio para operaciones de mantenimiento.</param>
+        /// <param name="unitOfWork">Unidad de trabajo para operaciones que involucran múltiples repositorios.</param>
         public MantenimientoService(IMantenimientoRepository mantenimientoRepository, IUnitOfWork unitOfWork)
         {
             _mantenimientoRepository = mantenimientoRepository;
             _unitOfWork = unitOfWork;
         }
 
+        /// <summary>
+        /// Obtiene una lista asincrónica de todos los mantenimientos.
+        /// </summary>
+        /// <returns>Una lista de instancias de Mantenimiento.</returns>
         public async Task<IEnumerable<Mantenimiento>> ListAsync()
         {
             return await _mantenimientoRepository.ListAsync();
         }
 
+        /// <summary>
+        /// Obtiene una lista asincrónica de los mantenimientos asociados a un propietario específico.
+        /// </summary>
+        /// <param name="propietarioId">Identificador del propietario.</param>
+        /// <returns>Una lista de instancias de Mantenimiento.</returns>
         public async Task<IEnumerable<Mantenimiento>> ListByPropietarioIdAsync(int propietarioId)
         {
             return await _mantenimientoRepository.FindByPropietarioIdAsync(propietarioId);
         }
 
+        /// <summary>
+        /// Obtiene una lista asincrónica de los mantenimientos asociados a un arrendatario específico.
+        /// </summary>
+        /// <param name="arrendatarioId">Identificador del arrendatario.</param>
+        /// <returns>Una lista de instancias de Mantenimiento.</returns>
         public async Task<IEnumerable<Mantenimiento>> ListByArrendatarioIdAsync(int arrendatarioId)
         {
             return await _mantenimientoRepository.FindByArrendatarioIdAsync(arrendatarioId);
         }
 
+        /// <summary>
+        /// Guarda un mantenimiento de manera asincrónica en la base de datos.
+        /// </summary>
+        /// <param name="mantenimiento">Datos del mantenimiento a guardar.</param>
+        /// <returns>Respuesta de la operación con el mantenimiento guardado o un error.</returns>
         public async Task<MantenimientoResponse> SaveAsync(Mantenimiento mantenimiento)
         {
             try
@@ -46,6 +76,12 @@ public class MantenimientoService : IMantenimientoService
             }
         }
 
+        /// <summary>
+        /// Actualiza un mantenimiento existente de manera asincrónica en la base de datos.
+        /// </summary>
+        /// <param name="mantenimientoId">Identificador del mantenimiento a actualizar.</param>
+        /// <param name="mantenimiento">Datos del mantenimiento con las actualizaciones.</param>
+        /// <returns>Respuesta de la operación con el mantenimiento actualizado o un error.</returns>
         public async Task<MantenimientoResponse> UpdateAsync(int mantenimientoId, Mantenimiento mantenimiento)
         {
             var existingMantenimiento = await _mantenimientoRepository.FindByIdAsync(mantenimientoId);
@@ -72,6 +108,11 @@ public class MantenimientoService : IMantenimientoService
             }
         }
 
+        /// <summary>
+        /// Elimina un mantenimiento de manera asincrónica de la base de datos.
+        /// </summary>
+        /// <param name="mantenimientoId">Identificador del mantenimiento a eliminar.</param>
+        /// <returns>Respuesta de la operación con el mantenimiento eliminado o un error.</returns>
         public async Task<MantenimientoResponse> DeleteAsync(int mantenimientoId)
         {
             var existingMantenimiento = await _mantenimientoRepository.FindByIdAsync(mantenimientoId);
