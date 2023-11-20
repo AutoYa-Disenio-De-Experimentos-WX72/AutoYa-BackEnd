c8c52df74896914eb998d2a297f22e650e3f3a1b
Johan Moreno Vergara <U20201C105@UPC.EDU.PE>
2023-11-19 21:56:48 -0500

    XML Documentation - PropietarioService

--- AutoYa-Backend/AutoYa/Services/PropietarioService.cs
+++ AutoYa-Backend/AutoYa/Services/PropietarioService.cs
@@ -6,17 +6,29 @@ using AutoYa_Backend.Shared.Persistence.Repositories;
 
 namespace AutoYa_Backend.AutoYa.Services;
 
+/// <summary>
+/// Servicio que ofrece métodos para la gestión de propietarios en la aplicación.
+/// </summary>
 public class PropietarioService : IPropietarioService
 {
     private readonly IPropietarioRepository _propietarioRepository;
         private readonly IUnitOfWork _unitOfWork;
 
+        /// <summary>
+        /// Constructor que inicializa una nueva instancia del servicio de propietarios.
+        /// </summary>
+        /// <param name="propietarioRepository">Repositorio para realizar operaciones de propietarios.</param>
+        /// <param name="unitOfWork">Unidad de trabajo para la ejecución de operaciones transaccionales.</param>
         public PropietarioService(IPropietarioRepository propietarioRepository, IUnitOfWork unitOfWork)
         {
             _propietarioRepository = propietarioRepository;
             _unitOfWork = unitOfWork;
         }
 
+        /// <summary>
+        /// Obtiene una lista de todos los propietarios de forma asincrónica.
+        /// </summary>
+        /// <returns>Lista asincrónica de propietarios.</returns>
         public async Task<IEnumerable<Propietario>> ListAsync()
         {
             return await _propietarioRepository.ListAsync();
@@ -43,6 +55,11 @@ public class PropietarioService : IPropietarioService
             }
         }
 
+        /// <summary>
+        /// Guarda un nuevo propietario en la base de datos de forma asincrónica.
+        /// </summary>
+        /// <param name="propietario">Datos del propietario a guardar.</param>
+        /// <returns>Respuesta con el resultado de la operación de guardado.</returns>
         public async Task<PropietarioResponse> UpdateAsync(int id, Propietario propietario)
         {
             // Buscar el propietario existente por ID
@@ -77,6 +94,12 @@ public class PropietarioService : IPropietarioService
             }
         }
 
+        /// <summary>
+        /// Actualiza un propietario existente en la base de datos de forma asincrónica.
+        /// </summary>
+        /// <param name="id">Identificador del propietario a actualizar.</param>
+        /// <param name="propietario">Datos actualizados del propietario.</param>
+        /// <returns>Respuesta con el resultado de la operación de actualización.</returns>
         public async Task<PropietarioResponse> DeleteAsync(int id)
         {
             var existingPropietario = await _propietarioRepository.FindByIdAsync(id);
