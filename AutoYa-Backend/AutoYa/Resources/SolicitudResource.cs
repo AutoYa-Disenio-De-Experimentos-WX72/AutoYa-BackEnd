e5373f4c1e3aa2530c9697b9350634584d5a4088
Johan Moreno Vergara <U20201C105@UPC.EDU.PE>
2023-11-19 21:02:24 -0500

    XML Documentation - SolicitudResource

--- AutoYa-Backend/AutoYa/Resources/SolicitudResource.cs
+++ AutoYa-Backend/AutoYa/Resources/SolicitudResource.cs
@@ -1,9 +1,24 @@
 ﻿namespace AutoYa_Backend.AutoYa.Resources;
 
+/// <summary>
+/// Clase de recurso que representa una solicitud dentro de la aplicación.
+/// Se utiliza para transferir datos de solicitudes entre las capas de la aplicación.
+/// </summary>
 public class SolicitudResource
 {
+
+    /// <summary>
+    /// Identificador único de la solicitud.
+    /// </summary>
     public int Id { get; set; }
+
+    /// <summary>
+    /// Cuerpo de la solicitud que contiene el mensaje o la información principal de la misma.
+    /// </summary>
     public string Body { get; set; }
-    
+
+    /// <summary>
+    /// Identificador del alquiler asociado a la solicitud.
+    /// </summary>
     public int AlquilerId { get; set; }
 }
