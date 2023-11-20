1ad74f1a53d1db0f8e029e7c3fd879bfe99629fe
Johan Moreno Vergara <U20201C105@UPC.EDU.PE>
2023-11-19 20:18:32 -0500

    XML Documentation - SaveAlquilerResource

--- AutoYa-Backend/AutoYa/Resources/SaveAlquilerResource.cs
+++ AutoYa-Backend/AutoYa/Resources/SaveAlquilerResource.cs
@@ -1,13 +1,44 @@
 ﻿namespace AutoYa_Backend.AutoYa.Resources;
 
+/// <summary>
+/// Clase de recurso utilizada para guardar la información de un alquiler.
+/// Contiene las propiedades necesarias para crear o actualizar un alquiler.
+/// </summary>
 public class SaveAlquilerResource
 {
+
+    /// <summary>
+    /// Estado actual del alquiler.
+    /// </summary>
     public string Estado { get; set; }
+
+    /// <summary>
+    /// Fecha y hora de inicio del periodo de alquiler.
+    /// </summary>
     public DateTime Fecha_inicio { get; set; }
+
+    /// <summary>
+    /// Fecha y hora de fin del periodo de alquiler.
+    /// </summary>
     public DateTime Fecha_fin { get; set; }
+
+    /// <summary>
+    /// Costo total asociado al alquiler.
+    /// </summary>
     public int Costo_total { get; set; }
-    
+
+    /// <summary>
+    /// Identificador del vehículo que se alquila.
+    /// </summary>
     public int VehiculoId { get; set; }
+
+    /// <summary>
+    /// Identificador del propietario del vehículo.
+    /// </summary>
     public int PropietarioId { get; set; }
+
+    /// <summary>
+    /// Identificador del arrendatario que realiza el alquiler.
+    /// </summary>
     public int ArrendatarioId { get; set; }
 }
