20c1739b778d6b69603ece2e8743f5e043b75407
Johan Moreno Vergara <U20201C105@UPC.EDU.PE>
2023-11-19 20:02:36 -0500

    XML Documentation - MantenimientoResource

--- AutoYa-Backend/AutoYa/Resources/MantenimientoResource.cs
+++ AutoYa-Backend/AutoYa/Resources/MantenimientoResource.cs
@@ -1,13 +1,51 @@
 ﻿namespace AutoYa_Backend.AutoYa.Resources;
 
+/// <summary>
+/// Clase de recurso para la transferencia de datos de mantenimiento asociados con vehículos.
+/// </summary>
+
 public class MantenimientoResource
 {
+
+    /// <summary>
+    /// Identificador único del registro de mantenimiento.
+    /// </summary>
+    
     public int Id { get; set; }
+
+    /// <summary>
+    /// Tipo de problema al que se dirige el mantenimiento.
+    /// </summary>
+    
     public string TipoProblema { get; set; }
+
+    /// <summary>
+    /// Título del problema de mantenimiento.
+    /// </summary>
+    
     public string Titulo { get; set; }
+
+    /// <summary>
+    /// Descripción detallada del problema de mantenimiento.
+    /// </summary>
+    
     public string Descripcion { get; set; }
+
+    /// <summary>
+    /// URL de una imagen que representa visualmente el problema de mantenimiento.
+    /// </summary>
+    
     public string UrlImagen { get; set; }
+
+    /// <summary>
+    /// Identificador del arrendatario asociado con el registro de mantenimiento.
+    /// </summary>
     
     public int ArrendatarioId { get; set; }
+
+    /// <summary>
+    /// Identificador del propietario asociado con el registro de mantenimiento.
+    /// </summary>
+    
     public int PropietarioId { get; set; }
 }
