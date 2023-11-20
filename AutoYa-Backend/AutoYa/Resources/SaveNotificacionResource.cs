4e9e1bfa115d37df06cf534382475e2cdd30fa9f
Johan Moreno Vergara <U20201C105@UPC.EDU.PE>
2023-11-19 20:34:26 -0500

    XML Documentation - SaveNotificacionResource

--- AutoYa-Backend/AutoYa/Resources/SaveNotificacionResource.cs
+++ AutoYa-Backend/AutoYa/Resources/SaveNotificacionResource.cs
@@ -1,8 +1,22 @@
 ﻿namespace AutoYa_Backend.AutoYa.Resources;
 
+/// <summary>
+/// Clase de recurso utilizada para guardar la información necesaria al crear una nueva notificación.
+/// </summary>
 public class SaveNotificacionResource
 {
+    /// <summary>
+    /// Contenido o cuerpo de la notificación que se va a enviar.
+    /// </summary>
     public string Body { get; set; }
+
+    /// <summary>
+    /// Identificador del propietario a quien va dirigida o que emite la notificación.
+    /// </summary>
     public int PropietarioId { get; set; }
+
+    /// <summary>
+    /// Identificador del arrendatario a quien va dirigida o que emite la notificación.
+    /// </summary>
     public int ArrendatarioId { get; set; }
 }
