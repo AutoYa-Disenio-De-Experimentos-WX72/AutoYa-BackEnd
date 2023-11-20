6a12eb603ceda7cf7c962d82686bcfa21cf34994
Johan Moreno Vergara <U20201C105@UPC.EDU.PE>
2023-11-19 20:05:21 -0500

    XML Documentation - NotificacionResource

--- AutoYa-Backend/AutoYa/Resources/NotificacionResource.cs
+++ AutoYa-Backend/AutoYa/Resources/NotificacionResource.cs
@@ -1,10 +1,30 @@
 ﻿namespace AutoYa_Backend.AutoYa.Resources;
 
+/// <summary>
+/// Clase de recurso para la transferencia de datos de notificaciones.
+/// Contiene las propiedades que representan la información esencial de una notificación.
+/// </summary>
+
 public class NotificacionResource
 {
+
+    /// <summary>
+    /// Identificador único de la notificación.
+    /// </summary>
     public int Id { get; set; }
+
+    /// <summary>
+    /// Cuerpo de la notificación con el mensaje a ser transmitido al usuario.
+    /// </summary>
     public string Body { get; set; }
-    
+
+    /// <summary>
+    /// Identificador del propietario asociado a la notificación.
+    /// </summary>
     public int PropietarioId { get; set; }
+
+    /// <summary>
+    /// Identificador del arrendatario asociado a la notificación.
+    /// </summary>
     public int ArrendatarioId { get; set; }
 }
