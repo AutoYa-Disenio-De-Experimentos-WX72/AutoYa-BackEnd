aab55a504dc6919ff0caa20a960f7f88de8fae0f
Alonso Robles <alonsofernando1907@outlook.es>
2024-04-26 16:58:46 -0500

    Actualización de la Base de Datos

--- AutoYa-Backend/obj/Debug/net7.0/AutoYa-Backend.AssemblyInfo.cs
+++ AutoYa-Backend/obj/Debug/net7.0/AutoYa-Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AutoYa-Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be84064473c6c95379716d05c28f5b7fe8ebc82a")]
 [assembly: System.Reflection.AssemblyProductAttribute("AutoYa-Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AutoYa-Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
